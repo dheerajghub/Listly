50f4b1315b9a01b41bef63130cf9caac4f5cd15a
DHEERAJ <dheeraj@appscrip.co>
2023-09-16 18:13:46 +0530

    Initial Commit

---
+++ SwiftDataDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwiftDataDemo
+//
+//  Created by Dheeraj Kumar Sharma on 16/09/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
